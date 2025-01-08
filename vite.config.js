ef47f14ecbf3f9c9528ae19529e050f9787f1119
VAKACHARLA SATHYA NAGA SAI <113987762+vsathyanagasai@users.noreply.github.com>
2025-01-08 18:59:14 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
-
-export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/CamblooProject/', // GitHub repository name
-});
